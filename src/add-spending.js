e2cd604f0b81dcd7f439cc3d1912f46ccfcfa841
YunlinXie <xieyunlin11@gmail.com>
2020-09-27 00:22:40 -0700

    create submit spending page

--- src/add-spending.js
+++ src/add-spending.js
@@ -5,7 +5,9 @@ export default class AddSpending extends React.Component {
     constructor() {
         super();
         this.state = {
-            datestr: "2018-01-01"
+            date: "2018-01-01",
+            amount: "",
+            // amount: "0.00",
         }
     }
 
@@ -16,18 +18,46 @@ export default class AddSpending extends React.Component {
                 <div className={"spending-date"}>
                     <label htmlFor="spendingDate">Spending date:</label>
                     <input type="date" id="spendingDate" name="spendingDate"
-                           value={this.state.datestr}
+                           value={this.state.date}
                            onChange={
                                (e) => {
                                    let val = e.target.value;
                                    this.setState((state, props) => ({
-                                       datestr: val
+                                       date: val
                                    }));
                                }
                            }
                     />
                 </div>
-                <div>
+                <div className={"spending-amount"}>
+                    <label htmlFor="spendingAmount">Spending amount:</label>
+
+                    <input type="number" id="spendingAmount" name="spendingAmount"
+                           step={0.01}
+                           placeholder={"Input a spending value"}
+                           value={this.state.amount}
+
+                           onChange={
+                               (e) => {
+                                   console.log(this.state.amount);
+
+                                   let val = e.target.value;
+                                   this.setState((state, props) => ({
+                                       amount: val
+                                   }));
+                               }
+                           }
+                          />
+                </div>
+                <div className={"submit"}>
+                    <button onClick={() => {
+                        /*
+                            1.validate input
+                            2. submit spending by calling api
+                         */
+                    }}>
+                        Submit spending
+                    </button>
 
                 </div>
             </div>
