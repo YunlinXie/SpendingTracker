a79d7e40c1131923dfbd5d22a64d0dbc3325f816
YunlinXie <xieyunlin11@gmail.com>
2020-09-27 00:05:38 -0700

    add datepicker

--- src/add-spending.js
+++ src/add-spending.js
@@ -1,10 +1,38 @@
 import React from "react";
 
-export default function AddSpending() {
+export default class AddSpending extends React.Component {
 
-    return (
-        <div>
-            Add spending goes here
-        </div>
-    );
+    constructor() {
+        super();
+        this.state = {
+            datestr: "2018-01-01"
+        }
+    }
+
+
+    render() {
+        return (
+            <div className={"add-spending"}>
+                <div className={"spending-date"}>
+                    <label htmlFor="spendingDate">Spending date:</label>
+                    <input type="date" id="spendingDate" name="spendingDate"
+                           value={this.state.datestr}
+                           onChange={
+                               (e) => {
+                                   let val = e.target.value;
+                                   this.setState((state, props) => ({
+                                       datestr: val
+                                   }));
+                               }
+                           }
+                    />
+                </div>
+                <div>
+
+                </div>
+            </div>
+        );
+
+    }
+
 }
